40aef3d3fd054fe493b50d4b148cc26f20af1df8
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2025-05-18 19:27:13 +0800

    initial commit

---
+++ src/main/java/com/example/peasinapod/PeasinapodApplication.java
@@ -0,0 +1,13 @@
+package com.example.peasinapod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeasinapodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeasinapodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/peasinapod/PeasinapodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peasinapod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeasinapodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
